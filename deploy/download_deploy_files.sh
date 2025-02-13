7cd38f76390d864190ae1e63c386c8af59ac0e96
Esben Zeuthen <esbenzeuthen@gmail.com>
2025-02-13 00:05:52 +0000

    bug fix

--- deploy/download_deploy_files.sh
+++ deploy/download_deploy_files.sh
@@ -6,16 +6,23 @@ BASE_URL="https://raw.githubusercontent.com/EsbenZeuthen/telink_mesh-mqtt-gatewa
 # List of files to download
 FILES=(
     "mosquitto/mosquitto.conf"
-    "docker-compose.yml"
+    "docker-compose.yml"    
 )
 
 # Download each file from the repository
 for FILE in "${FILES[@]}"; do
+    # Extract the directory from the file path
+    DIR=$(dirname "$FILE")
+
+    # Create the directory if it doesn't exist
+    mkdir -p "$DIR"
+
+    # Construct the URL for the raw file
     URL="$BASE_URL/$FILE"
     
-    # Download the file to the current directory
-    echo "Downloading $FILE..."
-    wget -q "$URL" -O "./$(basename "$FILE")"
+    # Download the file to the appropriate directory
+    echo "Downloading $FILE to $DIR..."
+    wget -q "$URL" -O "./$FILE"
 done
 
 echo "Download complete!"
